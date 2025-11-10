ebed33608e34995a228c90b6e904180f26dd4b6c
Lachlan Cain <lachlan.cain@petermac.org>
2025-11-10 16:50:52 +1100

    Updated dependencies

--- R/ATAClone-package.R
+++ R/ATAClone-package.R
@@ -1,11 +1,11 @@
-#` @import ggplot2
-#` @import rtracklayer
-#` @import data.table
-#` @import Matrix
-#` @import MatrixGenerics
-#` @import GenomicRanges
-#` @import igraph
-#` @import transformGamPoi
-#` @import irlba
-#` @import scran
+#' @import ggplot2
+#' @import rtracklayer
+#' @import data.table
+#' @import Matrix
+#' @import MatrixGenerics
+#' @import GenomicRanges
+#' @import igraph
+#' @import transformGamPoi
+#' @import irlba
+#' @import scran
 NULL
